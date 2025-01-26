88479eedf4bb9a386d4133f335c2e6c74f93f5e8
Rifki <rifkiachmadfachrezy@gmail.com>
2025-01-26 20:45:27 +0700

    last

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,20 +1,6 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-// GET Todos
-export async function GET() {
-  try {
-    const data = await db.todo.findMany();
-    return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    console.error("Failed to fetch todos:", error);
-    return NextResponse.json(
-      { error: "failed to fetch data" },
-      { status: 500 }
-    );
-  }
-}
-
 // POST Todo
 export async function POST(req: NextRequest) {
   try {
