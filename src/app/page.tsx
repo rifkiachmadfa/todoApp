9d5e16bbcd95b91083d535acb21c83d790bdb87f
Rifki <rifkiachmadfachrezy@gmail.com>
2025-01-26 07:45:36 +0700

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,34 @@ interface Todo {
 }
 
 export default async function Home() {
-  // Fetch data inside the server component
-  const response = await fetch("http://localhost:3000/api");
-  const todos: Todo[] = await response.json();
+  try {
+    const apiUrl =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api"
+        : "/api";
+    const response = await fetch(apiUrl);
 
-  return (
-    <>
-      <div>
-        <Greeting />
-      </div>
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from API");
+    }
 
-      <div className="w-[1280px] min-h-[60px] bg-white min-h-[480px] rounded-2xl shadow-2xl">
-        <div className="p-10">
-          <ListTodos todos={todos} />
+    const todos: Todo[] = await response.json();
+
+    return (
+      <>
+        <div>
+          <Greeting />
         </div>
-      </div>
-    </>
-  );
+
+        <div className="w-[1280px] min-h-[60px] bg-white min-h-[480px] rounded-2xl shadow-2xl">
+          <div className="p-10">
+            <ListTodos todos={todos} />
+          </div>
+        </div>
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Failed to load todos.</div>;
+  }
 }
