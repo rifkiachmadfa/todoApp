0e6588246d9116773bebdaa72ba8ea2b9d9cbb06
Rifki <rifkiachmadfachrezy@gmail.com>
2025-01-26 20:21:05 +0700

    tes

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,21 +1,27 @@
 import { db } from "@/lib/db";
-
 import { NextRequest, NextResponse } from "next/server";
 
+// GET Todos
 export async function GET() {
   try {
     const data = await db.todo.findMany();
-    return NextResponse.json(data);
+    return NextResponse.json(data, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: "failed to fetch data", status: 500 });
+    console.error("Failed to fetch todos:", error);
+    return NextResponse.json(
+      { error: "failed to fetch data" },
+      { status: 500 }
+    );
   }
 }
 
+// POST Todo
 export async function POST(req: NextRequest) {
   try {
     const { title, description } = await req.json();
+
     if (!title || !description) {
-      return NextResponse.json({ error: "invalid input", status: 400 });
+      return NextResponse.json({ error: "invalid input" }, { status: 400 });
     }
 
     const data = await db.todo.create({
@@ -25,8 +31,9 @@ export async function POST(req: NextRequest) {
       },
     });
 
-    return NextResponse.json({ massage: "success", data });
+    return NextResponse.json({ message: "success", data }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ massage: "error", error });
+    console.error("Error creating todo:", error);
+    return NextResponse.json({ message: "error", error }, { status: 500 });
   }
 }
